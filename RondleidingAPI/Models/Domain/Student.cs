e7a402b49fb64930c2b20349bc5533839a22986e
Jovi De Croock <jovi.decroock.u8420@student.hogent.be>
2016-11-30 11:24:26 +0100

    -- Jovi is slecht met databanken :collision:  --

--- RondleidingAPI/Models/Domain/Student.cs
+++ RondleidingAPI/Models/Domain/Student.cs
@@ -12,7 +12,42 @@ namespace RondleidingAPI.Models
         public string HouseNumber { get; set; }
         public string Province { get; set; }
         public string City { get; set; }
-        public IList<Campus> PrefCampus { get; set; }
-        public IList<Training> PrefTraining { get; set; }
+        public virtual IList<Campus> PrefCampus { get; set; }
+        public virtual IList<Training> PrefTraining { get; set; }
+        public Student() { }
+        public Student(string street, string houseNumber, string province, string city, string name, string email)
+        {
+            Street = street;
+            HouseNumber = houseNumber;
+            Province = province;
+            City = city;
+            Name = name;
+            Email = email;
+            PrefCampus = new List<Campus>();
+            PrefTraining = new List<Training>();
+        }
+
+        public void addCampus(Campus c)
+        {
+            PrefCampus.Add(c);
+        }
+        public void removeCampus(string name)
+        {
+            Campus c = PrefCampus.Where(campus => campus.Name == name).FirstOrDefault();
+            if (c != null)
+                PrefCampus.Remove(c);
+        }
+
+        public void addTraining(Training t)
+        {
+            PrefTraining.Add(t);
+        }
+        public void removeTraining(string name)
+        {
+            Training t = PrefTraining.Where(training => training.Name == name).FirstOrDefault();
+            if (t != null)
+                PrefTraining.Remove(t);
+        }
+
     }
 }
