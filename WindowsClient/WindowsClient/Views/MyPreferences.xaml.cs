384f606589302ad2438470bb5c7b1cea5e9f2660
KEVINMSI\Kevin <kevin.schuddinck.v6468@student.hogent.be>
2016-12-19 19:49:07 +0100

    -- adding student works (JOVI) --

--- WindowsClient/WindowsClient/Views/MyPreferences.xaml.cs
+++ WindowsClient/WindowsClient/Views/MyPreferences.xaml.cs
@@ -5,7 +5,10 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Runtime.Serialization.Json;
+using System.Text;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -28,7 +31,7 @@ namespace WindowsClient.Views
     {
         private List<CheckBox> checkboxList = new List<CheckBox>();
         private List<Models.Training> trainings = new List<Models.Training>();
-
+        private List<RootObject> result;
         public MyPreferences()
         {
             this.InitializeComponent();
@@ -41,18 +44,84 @@ namespace WindowsClient.Views
             Frame.Navigate(typeof(MainPage));
         }
 
-        private void verzendGegevens_Click(object sender, RoutedEventArgs e)
+        private async void verzendGegevens_Click(object sender, RoutedEventArgs e)
         {
-            List<Models.Training> trainings = this.checkComboBox.SelectedItems;
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            MemoryStream stream1 = new MemoryStream();
+            DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(Student));
+            List<string> trainings = new List<string>();
+            List<string> campussen = new List<string>();
+            List<Models.Training> trainingsPost = new List<Models.Training>();
+            List<Campus> campussenPost = new List<Campus>();
+            if (listbox != null)
+            {
+
+                foreach (CheckBox c in this.listbox.Items)
+                {
+                    if ((bool)c.IsChecked)
+                    {
+                        string x = (string)c.Content;
+                        string[] y = x.Split('-');
+                        if (!trainings.Contains(y[1]))
+                        {
+                            trainings.Add(y[1].Trim());
+                        }
+                        if (!campussen.Contains(y[0]))
+                        {
+                            campussen.Add(y[0].Trim());
+                        }
+                    }
+                }
+            }
+            foreach (RootObject r in result)
+            {
+                if(campussen.Contains(r.Name.Trim()))
+                {
+                    /*OMZETTEN NAAR CAMPUS*/
+                    Campus c = new Models.Campus();
+                    c.CampusId = r.CampusId;
+                    c.City = r.City;
+                    c.Feed = r.Feed;
+                    c.HouseNumber = r.HouseNumber;
+                    c.Name = r.Name;
+                    c.Street = r.Street;
+                    c.Telephone = r.Telephone;
+                    c.Trainingen = r.Trainingen;
+                    campussenPost.Add(c);
+                }
+                foreach (Models.Training t in r.Trainingen)
+                {
+                    if (trainings.Contains(t.Name.Trim()))
+                    {
+                        Models.Training test = trainingsPost.Where(p => p.Name == t.Name).FirstOrDefault();
+                        if (test == null)
+                        {
+                            trainingsPost.Add(t);
+                        }
+                    }
+                }
+            }
+            Student nieuw = new Student();
+            nieuw.Province = county.Text;
+            nieuw.Name = "" + firstname.Text + lastname.Text;
+            nieuw.City = city.Text;
+            nieuw.Street = street.Text;
+            nieuw.HouseNumber = houseNumber.Text;
+            nieuw.PrefTraining = trainingsPost;
+            nieuw.PrefCampus = campussenPost;
+            nieuw.Email = email.Text;
+            string body = JsonConvert.SerializeObject(nieuw);
+            var result2 = await client.PostAsync("http://localhost:50103/api/Students", new StringContent(body, Encoding.UTF8, "application/json"));
+              
         }
 
         private async void fillCheckboxes()
         {
             HttpClient client = new HttpClient();
             string json = await client.GetStringAsync("http://localhost:50103/api/Campus");
-            var result = JsonConvert.DeserializeObject<List<RootObject>>(json);
-
-
+            result = JsonConvert.DeserializeObject<List<RootObject>>(json);
 
             foreach(RootObject root in result)
             {
@@ -61,23 +130,26 @@ namespace WindowsClient.Views
                     CheckBox cb = new CheckBox();
                     cb.Content = root.Name + " - " + t.Name;
                     checkboxList.Add(cb);
-                }     
-                foreach(Models.Training t in root.Trainingen)
-                {
-                    t.Campussen.Add(root.Name);
-                    t.stringCB = root.Name + " - " + t.Name;
-                    trainings.Add(t);
-                }           
+                }         
             }
-
-            checkComboBox.ItemsSource = checkboxList;
-            listbox.ItemsSource = checkboxList;
+            if(checkComboBox != null)
+                checkComboBox.ItemsSource = checkboxList;
+            if(listbox != null)
+                listbox.ItemsSource = checkboxList;
         }
 
-        private void test(object sender, RoutedEventArgs e)
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
         {
-            string x = "";
+            CheckBox x = (CheckBox)sender;
+            x = checkboxList.Find(y => y.Content == x.Content);
+            if ((bool)x.IsChecked)
+            {
+                x.IsChecked = false;
+            }
+            else
+            {
+                x.IsChecked = true;
+            }
         }
     }
-
 }
