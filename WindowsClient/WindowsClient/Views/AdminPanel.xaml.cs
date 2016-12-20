0329cea09f14adc18ccd1f22bc1bf2c35ab3d744
KEVINMSI\Kevin <kevin.schuddinck.v6468@student.hogent.be>
2016-12-20 13:55:01 +0100

    no message

--- WindowsClient/WindowsClient/Views/AdminPanel.xaml.cs
+++ WindowsClient/WindowsClient/Views/AdminPanel.xaml.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -12,6 +15,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using WindowsClient.Models;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -22,9 +26,50 @@ namespace WindowsClient.Views
     /// </summary>
     public sealed partial class AdminPanel : Page
     {
+        private List<Student> studenten;
+        private List<Post> posts;
         public AdminPanel()
         {
             this.InitializeComponent();
+            GetPosts();
+        }
+
+        public async void GetStudents()
+        {
+            studenten = new List<Student>();
+            HttpClient client = new HttpClient();
+            string json = await client.GetStringAsync("http://localhost:50103/api/Student");
+            var result = JsonConvert.DeserializeObject<List<Student>>(json);
+            foreach (Student p in result)
+            {
+                studenten.Add(p);
+            }
+        }
+
+        public async void GetPosts()
+        {
+            /*Miss bij Posts een date adden pff.... Meer Migrations euj*/
+            posts = new List<Post>();
+            HttpClient client = new HttpClient();
+            string json = await client.GetStringAsync("http://localhost:50103/api/Posts");
+            var result = JsonConvert.DeserializeObject<List<Post>>(json);
+            foreach (Post p in result)
+            {
+                TextBlock d = new TextBlock();
+                d.Text = p;
+                posts.Add(p);
+            }
+        }
+
+        public async void PostPosts(Post p)
+        {
+            HttpClient client = new HttpClient();
+            string body = JsonConvert.SerializeObject(p);
+            var result = await client.PostAsync("http://localhost:50103/api/Posts", new StringContent(body, Encoding.UTF8, "application/json"));
+            if (result.IsSuccessStatusCode == true)
+            {
+                //Succes!
+            }
         }
     }
 }
